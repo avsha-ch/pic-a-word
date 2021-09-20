5e955039278031174a9b0925cff7d5b1df099852
RonMordoch <mordoch.ron@gmail.com>
2021-09-20 16:51:03 +0300

    Fix missing #

--- app/src/main/java/huji/postpc/find/pic/aword/onboarding/ChooseLanguageAdapter.kt
+++ app/src/main/java/huji/postpc/find/pic/aword/onboarding/ChooseLanguageAdapter.kt
@@ -7,40 +7,8 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 import huji.postpc.find.pic.aword.R
 import huji.postpc.find.pic.aword.game.models.Language
-
-//
-//class ChooseLanguageAdapter(private val languages : List<Language>) : RecyclerView.Adapter<ChooseLanguageAdapter.LanguageViewHolder>()  {
-//
-//    class LanguageViewHolder(val view: View): RecyclerView.ViewHolder(view){
-//        val languageIconImgView : ImageView = view.findViewById(R.id.language_icon_img_view)
-//        val languageTextView : TextView = view.findViewById(R.id.language_name_text_view)
-//    }
-//
-//    override fun getItemCount(): Int = languages.size
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LanguageViewHolder {
-//        val view = LayoutInflater.from(parent.context).inflate(R.layout.language_item_view, parent, false)
-//        return LanguageViewHolder(view)
-//    }
-//
-//    override fun onBindViewHolder(holder: LanguageViewHolder, position: Int) {
-//        // Get context in order to get resources (R)
-//        val context = holder.itemView.context
-//        // Select the bound item in position, a Language object
-//        val language = languages[position]
-//        // Get language name and icon
-//        val languageNameResId = language.nameResId
-//        val languageIconResId = language.iconResId
-//        // Set name and icon to view holder
-//        holder.languageTextView.text = context.getString(languageNameResId)
-//        holder.languageIconImgView.setImageResource(languageIconResId)
-//    }
-//
-//}
-
 
 class LanguageAdapter(private val adapterContext: Context, private val languages: List<Language>) : ArrayAdapter<Language>(adapterContext, R.layout.language_item_view, languages) {
 
