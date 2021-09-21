113e6b086b7d60bdd886b39a3ded879ded759a8e
Avshalom <avsha.chai@gmail.com>
2021-09-21 13:53:12 +0300

    captured images now saved merged with template overlay

--- app/src/main/java/huji/postpc/find/pic/aword/game/play/PlayFragment.kt
+++ app/src/main/java/huji/postpc/find/pic/aword/game/play/PlayFragment.kt
@@ -4,10 +4,16 @@ import huji.postpc.find.pic.aword.game.*
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.Manifest
+import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
@@ -23,6 +29,7 @@ import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.net.toFile
+import androidx.core.view.drawToBitmap
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -41,6 +48,9 @@ import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import huji.postpc.find.pic.aword.*
+import java.io.File.separator
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class PlayFragment : Fragment(R.layout.fragment_game) {
 
@@ -318,39 +328,74 @@ class PlayFragment : Fragment(R.layout.fragment_game) {
         })
 
         // handle saving image
-        imageCapture?.takePicture(outputOptions, cameraExecutor, object : ImageCapture.OnImageSavedCallback {
+        val capturedImageBitmap = cameraViewFinder.bitmap
+        val templateImageBitmap = wordImgView.drawToBitmap()
+        val mergedImageBitmap = capturedImageBitmap!!.with(templateImageBitmap)
 
-            override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                val savedUri = output.savedUri ?: Uri.fromFile(photoFile)
-                Log.d(CAMERA_X_TAG, "Photo capture succeeded: $savedUri")
+        saveImage(mergedImageBitmap, requireContext(), "PicAWord")
+    }
 
-                // Implicit broadcasts will be ignored for devices running API level >= 24
-                // so if you only target API level 24+ you can remove this statement
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-                    requireActivity().sendBroadcast(
-                        Intent(android.hardware.Camera.ACTION_NEW_PICTURE, savedUri)
-                    )
-                }
+    private fun Bitmap.with(bmp: Bitmap): Bitmap {
+        // Create new bitmap based on the size and config of the old
+        val newBitmap: Bitmap = Bitmap.createBitmap(this)
 
+        // Instantiate a canvas and prepare it to paint to the new bitmap
+        val canvas = Canvas(newBitmap)
 
-                // If the folder selected is an external media directory, this is
-                // unnecessary but otherwise other apps will not be able to access our
-                // images unless we scan them using [MediaScannerConnection]
-                val mimeType = MimeTypeMap.getSingleton()
-                    .getMimeTypeFromExtension(savedUri.toFile().extension)
-                MediaScannerConnection.scanFile(
-                    context,
-                    arrayOf(savedUri.toFile().absolutePath),
-                    arrayOf(mimeType)
-                ) { _, uri ->
-                    Log.d(CAMERA_X_TAG, "Image capture scanned into media store: $uri")
-                }
+        // Draw the old bitmap onto of the new white one
+        canvas.drawBitmap(bmp, 0f, 0f, null)
+
+        return newBitmap
+    }
+
+    /// @param folderName can be your app's name
+    private fun saveImage(bitmap: Bitmap, context: Context, folderName: String) {
+        if (android.os.Build.VERSION.SDK_INT >= 29) {
+            val values = contentValues()
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/$folderName")
+            values.put(MediaStore.Images.Media.IS_PENDING, true)
+            // RELATIVE_PATH and IS_PENDING are introduced in API 29.
+
+            val uri: Uri? = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            if (uri != null) {
+                saveImageToStream(bitmap, context.contentResolver.openOutputStream(uri))
+                values.put(MediaStore.Images.Media.IS_PENDING, false)
+                context.contentResolver.update(uri, values, null, null)
             }
+        } else {
+            val directory = File(Environment.getExternalStorageDirectory().toString() + separator + folderName)
+            // getExternalStorageDirectory is deprecated in API 29
 
-            override fun onError(exception: ImageCaptureException) {
-                Log.e(CAMERA_X_TAG, "Photo capture failed: ${exception.message}", exception)
+            if (!directory.exists()) {
+                directory.mkdirs()
             }
-        })
+            val fileName = System.currentTimeMillis().toString() + ".png"
+            val file = File(directory, fileName)
+            saveImageToStream(bitmap, FileOutputStream(file))
+            val values = contentValues()
+            values.put(MediaStore.Images.Media.DATA, file.absolutePath)
+            // .DATA is deprecated in API 29
+            context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        }
+    }
+
+    private fun contentValues() : ContentValues {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        return values
+    }
+
+    private fun saveImageToStream(bitmap: Bitmap, outputStream: OutputStream?) {
+        if (outputStream != null) {
+            try {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                outputStream.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun allPermissionsGranted() =
